0a927568a6c0c7f1703893deaa4f119e778ed8b5
pengdonglai <pengdonglai@bytedance.com>
2024-05-19 09:10:33 +0800

    feat: v2 支持参数

--- functions/api/setu/v2.js
+++ functions/api/setu/v2.js
@@ -1,5 +1,8 @@
 import * as Realm from 'realm-web';
 
+const rawPixivDomainName = 'i.pximg.net';
+const defaultPixivProxy = 'i.pixiv.re';
+
 let App;
 
 export async function onRequest(context) {
@@ -24,6 +27,36 @@ export async function onRequest(context) {
     });
   };
 
+  // 读取参数
+  const url = new URL(request.url);
+  // 检查URL中是否包含 "favicon.ico"
+  if (url.pathname.includes("favicon.ico")) {
+    // 如果包含 "favicon.ico"，则返回404响应
+    return new Response('Not Found', {
+      status: 404,
+      statusText: 'Not Found',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    });
+  };
+
+  // https://developer.mozilla.org/zh-CN/docs/Web/API/URLSearchParams#%E6%96%B9%E6%B3%95
+  const searchParams = new URLSearchParams(url.search);
+
+  // // 删除了原有根据 ua 来识别设备的逻辑
+  // // 处理 landscape 参数
+  // if (searchParams.has("landscape")) {
+  //   const landscape = searchParams.get("landscape") === "1" ? 1 : 0;
+  //   validatedSqlParts.push("`landscape` = " + landscape);
+  // }
+
+  // // 处理 near_square 参数
+  // if (searchParams.has('near_square')) {
+  //   const nearSquare = parseInt(searchParams.get('near_square')) === 1 ? true : false;
+  //   validatedSqlParts.push(`near_square = ${nearSquare}`);
+  // }
+
   App = App || new Realm.App(env.MONGO_APP_NAME);
   let client
   try {
@@ -32,7 +65,7 @@ export async function onRequest(context) {
     client = user.mongoClient('mongodb-atlas');
   } catch (err) {
     return new Response('登录服务器时出错，请联系管理员', {
-      status: 404,
+      status: 502,
       headers: {
         'Content-Type': 'text/plain; charset=utf-8',
       },
@@ -49,6 +82,44 @@ export async function onRequest(context) {
     ])
   } catch (error) {
     return new Response('查询出错', {
+      status: 502,
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+      },
+    });
+  }
+
+  const rawResult = result[0];
+  if (!rawResult) {
+    return new Response('返回结果为空', {
+      status: 404,
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+      },
+    });
+  }
+  const processedResult = {
+    id: rawResult.id,
+    tags: rawResult.tags,
+    title: rawResult.title,
+    description: rawResult.description,
+    user: rawResult.user,
+    userId: rawResult.userId,
+    width: rawResult.fullWidth,
+    height: rawResult.fullHeight,
+  };
+
+  let imgUrl;
+
+  // 处理图片大小参数
+  const sizeParamName = searchParams.get('size');
+  if (!sizeParamName || !['original', 'regular', 'small', 'thumb'].includes(sizeParamName) || !rawResult[sizeParamName]) {
+    imgUrl = getImgUrl(rawResult);
+  } else {
+    imgUrl = rawResult[sizeParamName];
+  }
+  if (!imgUrl) {
+    return new Response('URL 查询为空', {
       status: 404,
       headers: {
         'Content-Type': 'text/plain; charset=utf-8',
@@ -56,35 +127,33 @@ export async function onRequest(context) {
     });
   }
 
-  return new Response(JSON.stringify(result[0]));
+  // 处理代理参数
+  const proxyName = searchParams.get('proxy');
+  if (proxyName === 0 || proxyName === null) {
+    // pass
+  } else if (!proxyName) {
+    imgUrl?.replace(rawPixivDomainName, defaultPixivProxy);
+  } else {
+    let target;
+    try {
+      // 传过来的是 URL
+      const innerUrl = new URL(proxyName);
+      target = innerUrl.hostname;
+    } catch {
+      // 传过来的不是 URL
+      target = proxyName;
+    } finally {
+      imgUrl?.replace(rawPixivDomainName, target ?? defaultPixivProxy);
+    }
+  }
 
-  // // 读取参数
-  // const url = new URL(request.url);
-  // // 检查URL中是否包含 "favicon.ico"
-  // if (url.pathname.includes("favicon.ico")) {
-  //   // 如果包含 "favicon.ico"，则返回404响应
-  //   return new Response('Not Found', {
-  //     status: 404,
-  //     statusText: 'Not Found',
-  //     headers: {
-  //       'Content-Type': 'text/plain',
-  //     },
-  //   });
-  // }
+  // imgUrl 处理完，放到 url 参数中
+  processedResult.url = imgUrl;
 
-  // // https://developer.mozilla.org/zh-CN/docs/Web/API/URLSearchParams#%E6%96%B9%E6%B3%95
-  // const searchParams = new URLSearchParams(url.search);
+  return new Response(JSON.stringify(processedResult));
+};
 
-  // // 删除了原有根据 ua 来识别设备的逻辑
-  // // 处理 landscape 参数
-  // if (searchParams.has("landscape")) {
-  //   const landscape = searchParams.get("landscape") === "1" ? 1 : 0;
-  //   validatedSqlParts.push("`landscape` = " + landscape);
-  // }
-
-  // // 处理 near_square 参数
-  // if (searchParams.has('near_square')) {
-  //   const nearSquare = parseInt(searchParams.get('near_square')) === 1 ? true : false;
-  //   validatedSqlParts.push(`near_square = ${nearSquare}`);
-  // }
+const getImgUrl = (rawData) => {
+  const { original, regular, small, thumb } = rawData || {};
+  return original || regular || small || thumb;
 };
